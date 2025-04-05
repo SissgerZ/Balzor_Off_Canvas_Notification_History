47b304ba29d1d9e612acbb8afd78ce653c7be215
SissgerZ <fuessinger94@web.de>
2025-04-05 18:10:22 +0200

    Add blazor bootstrap

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 using Balzor_Off_Canvas_Notification_History.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddBlazorBootstrap();
 
 var app = builder.Build();
 
